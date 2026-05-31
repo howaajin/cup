ad45200fa01997dc0adb6022b82b62d5ee22f8d6
howaajin <howaajin@outlook.com>
2026-05-31 13:18:31 +0800

    fix: use cc instead of gcc in bootstrap.sh, reject -arch x86 on macOS

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,11 @@ while [ $# -gt 0 ]; do
     shift
 done
 
+if [ "$(uname -s)" = "Darwin" ] && [ "$ARCH" = "x86" ]; then
+    echo "error: -arch x86 is not supported on macOS (32-bit support removed since macOS 10.15)"
+    exit 1
+fi
+
 if [ "$ARCH" = "x86" ]; then
     CLANG_FLAGS="-m32"
     LINK_FLAGS="-m32"
@@ -33,11 +38,11 @@ fi
 
 mkdir -p build
 
-gcc $CLANG_FLAGS src/core/hash_gen.c -g -O0 -Isrc -fms-extensions -Wno-microsoft-anon-tag -o build/hash_gen \
+cc $CLANG_FLAGS src/core/hash_gen.c -g -O0 -Isrc -fms-extensions -Wno-microsoft-anon-tag -o build/hash_gen \
     -Wno-deprecated-declarations
 
 build/hash_gen -o src/core/hash.h src/core/hash_gen.c
 
-gcc $CLANG_FLAGS build.c src/core/build.c src/cup/build.c src/cup/in_repo.c src/cup/c_toolchain/build.c \
+cc $CLANG_FLAGS build.c src/core/build.c src/cup/build.c src/cup/in_repo.c src/cup/c_toolchain/build.c \
     src/cup/bootstrap.c -D_GNU_SOURCE -g -O0 -fms-extensions -Wno-microsoft-anon-tag -Isrc -o cup \
     -Wno-deprecated-declarations $LINK_FLAGS
