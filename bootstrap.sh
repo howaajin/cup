071a714614362666c348fb9e658e8d0790fcc1eb
howaajin <howaajin@outlook.com>
2026-06-07 00:27:24 +0800

    fix: respect CC environment variable in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -e
 ARCH=""
 CLANG_FLAGS=""
 LINK_FLAGS=""
+CC="${CC:-cc}"
 
 while [ $# -gt 0 ]; do
     case "$1" in
@@ -38,11 +39,11 @@ fi
 
 mkdir -p build
 
-cc $CLANG_FLAGS src/core/hash_gen.c -g -O0 -Isrc -fms-extensions -Wno-microsoft-anon-tag -o build/hash_gen \
+$CC $CLANG_FLAGS src/core/hash_gen.c -g -O0 -Isrc -fms-extensions -Wno-microsoft-anon-tag -o build/hash_gen \
     -Wno-deprecated-declarations
 
 build/hash_gen -o src/core/hash.h src/core/hash_gen.c
 
-cc $CLANG_FLAGS build.c src/core/build.c src/cup/build.c src/cup/in_repo.c src/cup/c_toolchain/build.c \
+$CC $CLANG_FLAGS build.c src/core/build.c src/cup/build.c src/cup/in_repo.c src/cup/c_toolchain/build.c \
     src/cup/bootstrap.c -D_GNU_SOURCE -g -O0 -fms-extensions -Wno-microsoft-anon-tag -Isrc -o cup \
     -Wno-deprecated-declarations $LINK_FLAGS
